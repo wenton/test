fd4cb9bef766f1ec1f2d62463cbbe1085a41869d
Wenton <wenton.alex@gmail.com>
2014-03-31 14:24:46 +0400

    models.py mistakes

--- djangoenv/bin/firstapp/article/models.py
+++ djangoenv/bin/firstapp/article/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Article(models.Model):
     class Meta():
         db_table = "article"
-        article_title = models.CharField(max_length = 200)
-        article_text = models.TextField()
-        article_date = models.DateTimeField()
-        article_likes = models.IntegerField()
+    article_title = models.CharField(max_length=200)
+    article_text = models.TextField()
+    article_date = models.DateTimeField()
+    article_likes = models.IntegerField()
