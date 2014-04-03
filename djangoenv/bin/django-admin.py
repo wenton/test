00b3dbbe236f3156b490664059d209791c8290e0
Wenton <wenton.alex@gmail.com>
2014-04-03 20:37:43 +0400

    first django

---
+++ djangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/wenton/IdeaProjects/test/djangoenv/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.6.2','django-admin.py'
+__requires__ = 'Django==1.6.2'
+import pkg_resources
+pkg_resources.run_script('Django==1.6.2', 'django-admin.py')
